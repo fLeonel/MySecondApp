87c2dff08c29a3902c906735bb417ce9e7e2602b
Dennis Ramirez <dr04860@gmail.com>
2024-09-26 14:41:35 -0600

    creating a model for messages

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/infrastructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer {
   final _dio = Dio();
@@ -7,9 +8,11 @@ class GetYesNoAnswer {
   Future<Message> getAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
 
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+
     return Message(
-        text: response.data['answer'],
+        text: yesNoModel.answer,
         fromWho: FromWho.hers,
-        imageURL: response.data['image']);
+        imageURL: yesNoModel.image);
   }
 }
--- lib/infrastructure/models/yes_no_model.dart
+++ lib/infrastructure/models/yes_no_model.dart
@@ -7,6 +7,7 @@ class YesNoModel {
 
   YesNoModel({required this.answer, required this.forced, required this.image});
 
+  //el factory es para crear una nueva instancia.
   factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
       answer: json['anwer'], forced: json['forced'], image: json['image']);
 }
